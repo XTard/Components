655c227970de3711b4185ef4a4ff498d39c021f6
XTard <ickomcdevill@gmail.com>
2019-09-19 02:24:12 +0300

    Update on the work

--- search/script.js
+++ search/script.js
@@ -1,12 +1,10 @@
 function filterFunction(event) {
 	let container, input, filter, li, input_val;
-	container = $(this).closest(".searchable");
-	input_val = $(this).val().toUpperCase();
-	console.log(event);
+	container = $(this);
+	input_val = $(this).children("input").val().toUpperCase();
 
-	if (["ArrowUp", "ArrowDown", "Enter"].indexOf(event.key) != -1) {
-		console.log("Asd");
-		keyControl(event, container)
+	if (["Enter", "ArrowUp", "ArrowDown"].indexOf(event.key) > -1) {
+		eventControl(event, container)
 	} else {
 		li = container.find("ul li");
 		li.each(function (i, obj) {
@@ -22,30 +20,26 @@ function filterFunction(event) {
 	}
 }
 
-function keyControl(e, container) {
-	if (e.key == "ArrowDown") {
-
+function eventControl(ev, container) {
+	if (ev.key == "ArrowDown") {
 		if (container.find("ul li").hasClass("selected")) {
 			if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length) {
 				container.find("ul li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
 			}
-
 		} else {
 			container.find("ul li:first-child").addClass("selected");
+			// container.find("input").blur();
+			// container.find("ul").focus();
 		}
-
-	} else if (e.key == "ArrowUp") {
-
+	} else if (ev.key == "ArrowUp") {
 		if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0) {
 			container.find("ul li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
 		}
-	} else if (e.key == "Enter") {
+	} else if (ev.key == "Enter") {
 		container.find("input").blur();
-		console.log("asd");
+		container.find("ul").focus();
 		onSelect(container.find("ul li.selected").text())
 	}
-
-	console.log("asd");
 	container.find("ul li.selected")[0].scrollIntoView({
 		behavior: "smooth",
 		block: "center"
@@ -56,22 +50,62 @@ function onSelect(val) {
 	console.log(val);
 }
 
-$(".searchable ul li").hover(function () {
-	$(this).closest(".searchable").find("ul li.selected").removeClass("selected");
-	$(this).addClass("selected");
-	// console.log("hover");
+// $(".searchable ul li").hover(function () {
+// 	$(this).closest(".searchable").find("ul li.selected").removeClass("selected");
+// 	$(this).addClass("selected");
+// 	// console.log("hover");
+// });
+
+$(".searchable").on('keyup', filterFunction);
+$("ul").on('scroll', function () {
+	console.log("scroll");
+});
+$("ul").on('wheeel', function (e) {
+	// console.log("wheel");
+	let container, input, filter, li, input_val;
+	container = $(this);
+	// input_val = $(this).children("input").val().toUpperCase();
+	if (typeof e.originalEvent.wheelDelta == 'number') {
+		if (e.originalEvent.wheelDelta > 0) { // UP
+			// console.log("up");
+			if (container.find("li:visible").index(container.find(".selected")) > 0)
+				container.find("li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
+		} else if (e.originalEvent.wheelDelta < 0) { // DOWN
+			// console.log("down");
+			if (container.find("ul li").hasClass("selected")) {
+				if (container.find("li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length)
+					container.find("li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
+			} else {
+				container.find("ul li:first-child").addClass("selected");
+			}
+		}
+		container.find("li.selected")[0].scrollIntoView({
+			behavior: "smooth",
+			block: "center"
+		});
+	}
+		// else if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+	// 	if (e.originalEvent.detail > 0) { // DOWN
+	// 		console.log("down");
+	// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length) {
+	// 			container.find("ul li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
+	// 		}
+	// 	} else if (e.originalEvent.detail < 0) { // UP
+	// 		console.log("up");
+	// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0) {
+	// 			container.find("ul li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
+	// 		}
+	//   }
+	// }
 });
 
-$("#gosho").on('keyup', filterFunction);
-
-// $(".searchable input").focus(function () {
-// 	console.log("focused");
-//     $(this).closest(".searchable").find("ul").show();
-//     $(this).closest(".searchable").find("ul li").show();
-// });
 
 // $(".searchable input").blur(function () {
 // 	console.log("blurred");
-//     let that = this;
-//     $(that).closest(".searchable").find("ul").hide();
+// });
+//
+// $(".searchable ul").focus(function () {
+// 	console.log("focused ul");
+//     // $(this).closest(".searchable").find("ul").show();
+//     // $(this).closest(".searchable").find("ul li").show();
 // });
