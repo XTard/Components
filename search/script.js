6c7a3a05cae89d80eaa6fc2ca5f301f2fdd19e3a
XTard <ickomcdevill@gmail.com>
2019-09-25 12:49:21 +0300

    Wawawiwa commit. A lot of work has to be done.

--- search/script.js
+++ search/script.js
@@ -1,54 +1,161 @@
-function filterFunction(event) {
-	let container, input, filter, li, input_val;
-	container = $(this);
-	input_val = $(this).children("input").val().toUpperCase();
+var container = jQuery("#searchBar");
+var lastY = 0;
 
-	if (["Enter", "ArrowUp", "ArrowDown"].indexOf(event.key) > -1) {
-		eventControl(event, container)
-	} else {
-		li = container.find("ul li");
-		li.each(function (i, obj) {
-			if ($(this).text().toUpperCase().indexOf(input_val) > -1) {
-				$(this).show();
-			} else {
-				$(this).hide();
-			}
-		});
+function filterDropdown () {
+	var input, li, input_val;
+	input_val = container.children("input").val().toLowerCase();
 
-		container.find("ul li").removeClass("selected");
-		container.find("ul li:visible").first().addClass("selected");
-	}
-}
-
-function eventControl(ev, container) {
-	if (ev.key == "ArrowDown") {
-		if (container.find("ul li").hasClass("selected")) {
-			if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length) {
-				container.find("ul li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
-			}
+	li = container.find("ul li");
+	li.each(function (i, obj) {
+		if ($(this).text().toLowerCase().indexOf(input_val) > -1) {
+			$(this).show();
 		} else {
-			container.find("ul li:first-child").addClass("selected");
-			// container.find("input").blur();
-			// container.find("ul").focus();
+			$(this).hide();
 		}
-	} else if (ev.key == "ArrowUp") {
-		if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0) {
-			container.find("ul li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
-		}
-	} else if (ev.key == "Enter") {
-		container.find("input").blur();
-		container.find("ul").focus();
-		onSelect(container.find("ul li.selected").text())
-	}
-	container.find("ul li.selected")[0].scrollIntoView({
-		behavior: "smooth",
-		block: "center"
 	});
+
+	container.find("ul li.selected").removeAttr("selected");
+}
+function customScroll (direction) {
+container.find("ul li.selected").removeAttr("class").nextAll("li").not('[style*="display: none"]').first().addClass("selected");
+
+container.find("ul li.selected").removeAttr("class").prevAll("li").not('[style*="display: none"]').first().addClass("selected");
+}
+function eventControl (e) {
+	// console.log(e.type);
+	// left: 37, up: 38, right: 39, down: 40,
+	// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+	if (e.type == "input" || e.type == "keydown") {
+		// console.log("key " + e.key + " which " + e.which);
+		// switch (e.key) {
+		// 	case "ArrowUp":
+		// 		e.preventDefault();
+		// 		console.log("au");
+		// 		break;
+		// 	case "ArrowDown":
+		// 		e.preventDefault();
+		// 		console.log("ad");
+		// 		break;
+		// 	// case "ArrowLeft":
+		// 	// 	e.preventDefault();
+		// 	// 	console.log("al");
+		// 	// 	break;
+		// 	// case "ArrowRight":
+		// 	// 	e.preventDefault();
+		// 	// 	console.log("ar");
+		// 	// 	break;
+		// 	case "Enter":
+		// 		console.log("enter");
+		// 		break;
+		// 	default:
+				filterDropdown();
+		// }
+
+		// if (e.key == "ArrowDown") {
+		// 	if (container.find("ul li").hasClass("selected")) {
+		// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length)
+		// 			container.find("ul li.selected").removeClass("selected").nextAll("li").not('[style*="display: none"]').first().addClass("selected");
+		// 	} else {
+		// 		container.find("ul li:first-child").addClass("selected");
+		// 	}
+		// } else if (e.key == "ArrowUp") {
+		// 	if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0)
+		// 		container.find("ul li.selected").removeClass("selected").prevAll("li").not('[style*="display: none"]').first().addClass("selected");
+		// } else if (e.key == "Enter") {
+		// 	// container.find("input").blur();
+		// 	// container.find("ul").focus();
+		// 	container.find("ul li:visible").first().addClass("selected");
+		// 	onSelect(container.find("ul li.selected").text())
+		// }
+	}
+	else if (e.type == "wheel") {
+		e.preventDefault();
+		let deltaY = e.originalEvent.deltaY,
+			detail = e.originalEvent.detail;
+
+		if (container.find("ul li:visible").hasClass("selected") === false)
+			container.find("ul li:visible").first().addClass("selected");
+		console.log("detail " + e.originalEvent.detail + " Y - " + e.originalEvent.deltaY);
+		// DOWN ++
+		// UP --
+		if (typeof deltaY === 'number') {
+			if (deltaY > 0) {
+				console.log("down");
+			} else if (deltaY < 0) {
+				console.log("up");
+			}
+		} else if (typeof detail === 'number' && detail !== 0) {
+			if (deltaY > 0) {
+				console.log("down");
+			} else if (deltaY < 0) {
+				console.log("up");
+			}
+		}
+		// if (typeof e.originalEvent.wheelDelta == 'number') {
+		// 	console.log("asd");
+		// 	if (e.originalEvent.wheelDelta > 0) { // UP
+		// 		if (container.find("li:visible").index(container.find(".selected")) > 0)
+		// 			container.find("li.selected").removeClass("selected").prevAll("li").not('[style*="display: none"]').first().addClass("selected");
+		// 	} else if (e.originalEvent.wheelDelta < 0) { // DOWN
+		// 		if (container.find("ul li").hasClass("selected")) {
+		// 			if (container.find("li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length)
+		// 				container.find("li.selected").removeClass("selected").nextAll("li").not('[style*="display: none"]').first().addClass("selected");
+		// 		} else {
+		// 			container.find("ul li:first-child").addClass("selected");
+		// 		}
+		// 	}
+		// }
+		// else if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+		// 	console.log("qwe");
+		// 	if (e.originalEvent.detail > 0) { // DOWN
+		// 		if (container.find("ul li").hasClass("selected")) {
+		// 			if (container.find("li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length)
+		// 				container.find("li.selected").removeClass("selected").nextAll("li").not('[style*="display: none"]').first().addClass("selected");
+		// 		} else {
+		// 			container.find("ul li:first-child").addClass("selected");
+		// 		}
+		// 	} else if (e.originalEvent.detail < 0) { // UP
+		// 		if (container.find("li:visible").index(container.find(".selected")) > 0)
+		// 			container.find("li.selected").removeClass("selected").prevAll("li").not('[style*="display: none"]').first().addClass("selected");
+		// 	}
+		// }
+	}
+	else if (e.type == "touchmove") {
+		e.preventDefault();
+		// $(document).on('touchmove', function (e) {
+			var currentY = e.originalEvent.touches[0].clientY;
+			$("#qwe").text(e.originalEvent.touches[0].clientY);
+		     if (currentY > lastY) {
+			 	$("#kek").text("down");
+		         // moved down
+		     } else if(currentY < lastY){
+		         // moved up
+				 $("#kek").text("up");
+		     }
+		     lastY = currentY;
+			 /* console.log(e.originalEvent.touches[0].clientY); */
+		// });
+	}
+	// container.find("ul li.selected")[0].scrollIntoView({
+	// 	behavior: "smooth",
+	// 	block: "center"
+	// });
 }
 
 function onSelect(val) {
 	console.log(val);
 }
+
+$("#searchBar").on("input keydown wheel touchmove", eventControl);
+
+$("ul").focus(function (){
+	$("input").focus();
+});
+
+// $("*").on("scroll", function (e) {
+// 	// e.stopImmediatePropagation();
+// 	console.log("aaa");
+// });
 
 // $(".searchable ul li").hover(function () {
 // 	$(this).closest(".searchable").find("ul li.selected").removeClass("selected");
@@ -56,56 +163,45 @@ function onSelect(val) {
 // 	// console.log("hover");
 // });
 
-$(".searchable").on('keyup', filterFunction);
-$("ul").on('scroll', function () {
-	console.log("scroll");
-});
-$("ul").on('wheeel', function (e) {
-	// console.log("wheel");
-	let container, input, filter, li, input_val;
-	container = $(this);
-	// input_val = $(this).children("input").val().toUpperCase();
-	if (typeof e.originalEvent.wheelDelta == 'number') {
-		if (e.originalEvent.wheelDelta > 0) { // UP
-			// console.log("up");
-			if (container.find("li:visible").index(container.find(".selected")) > 0)
-				container.find("li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
-		} else if (e.originalEvent.wheelDelta < 0) { // DOWN
-			// console.log("down");
-			if (container.find("ul li").hasClass("selected")) {
-				if (container.find("li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length)
-					container.find("li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
-			} else {
-				container.find("ul li:first-child").addClass("selected");
-			}
-		}
-		container.find("li.selected")[0].scrollIntoView({
-			behavior: "smooth",
-			block: "center"
-		});
-	}
-		// else if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
-	// 	if (e.originalEvent.detail > 0) { // DOWN
-	// 		console.log("down");
-	// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length) {
-	// 			container.find("ul li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
-	// 		}
-	// 	} else if (e.originalEvent.detail < 0) { // UP
-	// 		console.log("up");
-	// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0) {
-	// 			container.find("ul li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
-	// 		}
-	//   }
-	// }
-});
-
-
-// $(".searchable input").blur(function () {
-// 	console.log("blurred");
+// $("#searchBar input").on('input', filterFunction);
+// $("ul").on('scroll', function () {
+// 	console.log("scroll");
 // });
-//
-// $(".searchable ul").focus(function () {
-// 	console.log("focused ul");
-//     // $(this).closest(".searchable").find("ul").show();
-//     // $(this).closest(".searchable").find("ul li").show();
+// $("ul").on('wheel', function (e) {
+// 	// console.log("wheel");
+// 	let container, input, filter, li, input_val;
+	// container = $(this);
+	// // input_val = $(this).children("input").val().toUpperCase();
+	// if (typeof e.originalEvent.wheelDelta == 'number') {
+	// 	if (e.originalEvent.wheelDelta > 0) { // UP
+	// 		// console.log("up");
+	// 		if (container.find("li:visible").index(container.find(".selected")) > 0)
+	// 			container.find("li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
+	// 	} else if (e.originalEvent.wheelDelta < 0) { // DOWN
+	// 		// console.log("down");
+	// 		if (container.find("ul li").hasClass("selected")) {
+	// 			if (container.find("li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length)
+	// 				container.find("li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
+	// 		} else {
+	// 			container.find("ul li:first-child").addClass("selected");
+	// 		}
+	// 	}
+	// 	container.find("li.selected")[0].scrollIntoView({
+	// 		behavior: "smooth",
+	// 		block: "center"
+	// 	});
+	// }
+		// else if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+		// 	if (e.originalEvent.detail > 0) { // DOWN
+		// 		console.log("down");
+		// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length) {
+		// 			container.find("ul li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
+		// 		}
+		// 	} else if (e.originalEvent.detail < 0) { // UP
+		// 		console.log("up");
+		// 		if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0) {
+		// 			container.find("ul li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
+		// 		}
+		// 	}
+		// }
 // });
